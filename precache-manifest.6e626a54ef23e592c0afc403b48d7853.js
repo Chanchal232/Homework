013244ce418502f83654ec702000faa16469cb1c
chanchalsaluja <chanchalsaluja32@gmail.com>
2020-08-06 17:51:47 +0530

    Updates

---
+++ precache-manifest.6e626a54ef23e592c0afc403b48d7853.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631c3d04e13b3adc54298f036acec493",
+    "url": "/HomeAssignments/index.html"
+  },
+  {
+    "revision": "1fbf3fd0e0f09b20d70b",
+    "url": "/HomeAssignments/static/css/2.a09026a1.chunk.css"
+  },
+  {
+    "revision": "105aaa8f086b539129f6",
+    "url": "/HomeAssignments/static/css/main.563ed669.chunk.css"
+  },
+  {
+    "revision": "1fbf3fd0e0f09b20d70b",
+    "url": "/HomeAssignments/static/js/2.6c95ee8f.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/HomeAssignments/static/js/2.6c95ee8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105aaa8f086b539129f6",
+    "url": "/HomeAssignments/static/js/main.3fd19cd7.chunk.js"
+  },
+  {
+    "revision": "9e216e20f72794057360",
+    "url": "/HomeAssignments/static/js/runtime-main.05f280f4.js"
+  }
+]);
